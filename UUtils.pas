6dc5d2625e4587fdae53605509b14d3cfd72adad
DarknessHunterr <lucarbass@live.com>
2020-05-20 01:43:59 -0300

    Created for Utils methods, there is just email validation for now

---
+++ UUtils.pas
@@ -0,0 +1,29 @@
+unit UUtils;
+
+interface
+
+uses System.RegularExpressions;
+
+type
+  TUtils = class
+    public
+      function ValidateEmail(email : string) : Boolean;
+  end;
+
+implementation
+
+{ TUtils }
+
+function TUtils.ValidateEmail(email: string): Boolean;
+const EMAILREGEX = '^((?>[a-zA-Z\d!#$%&''*+\-/=?^_`{|}~]+\x20*|"((?=[\x01-\x7f])'
+             +'[^"\\]|\\[\x01-\x7f])*"\x20*)*(?<angle><))?((?!\.)'
+             +'(?>\.?[a-zA-Z\d!#$%&''*+\-/=?^_`{|}~]+)+|"((?=[\x01-\x7f])'
+             +'[^"\\]|\\[\x01-\x7f])*")@(((?!-)[a-zA-Z\d\-]+(?<!-)\.)+[a-zA-Z]'
+             +'{2,}|\[(((?(?<!\[)\.)(25[0-5]|2[0-4]\d|[01]?\d?\d))'
+             +'{4}|[a-zA-Z\d\-]*[a-zA-Z\d]:((?=[\x01-\x7f])[^\\\[\]]|\\'
+             +'[\x01-\x7f])+)\])(?(angle)>)$';
+begin
+  Result := TRegEx.IsMatch(email, EMAILREGEX);
+end;
+
+end.
